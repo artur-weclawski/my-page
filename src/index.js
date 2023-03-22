72a54bde71ee00446375467f8a51b2c5f8793e1f
Artur-Weclawski <weclawski.artur@gmail.com>
2023-03-22 11:23:31 +0100

    Removed last fix

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 require ('./script.js');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <HashRouter>
+     <BrowserRouter>
       <Routes>
         <Route path="/my-page/*" element={ <App /> }>
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 reportWebVitals();
